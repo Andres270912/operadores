52baaf5bf12a861d61b24d89261759fa0d757c4d
Harrinson Martinez <andresmartinez.ml2709@gmail.com>
2024-09-26 16:49:16 -0500

    se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresDeAsigancion.py
@@ -0,0 +1,20 @@
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1 += 3
+print(f"Asignacion con suma: {var1}")
+var2 -= 3
+var3 *= 3
+var4 /= 3
+var5 %= 3
+var6 //= 3
+
+print(f"Asignacion con resta: {var2}")
+print(f"Asignacion con multiplicacion: {var3}")
+print(f"Asignacion con divicion: {var4}")
+print(f"Asignacion con residuo: {var5}")
+print(f"Asignacion con division entera: {var6}")
---
+++ operadoresProject/OperativosAritmetoco.py
@@ -0,0 +1,23 @@
+#ejemplos utilizando operadores aritmeticos
+numero1 = int ( input("Ingrese el numero 1 "))
+numero2 = int  (input("Ingrese el numero 2 "))
+
+suma = numero1 + numero2
+print("la suma es ",str(suma))
+
+#Aritmeticos
+resta = numero1 - numero2
+division = numero1 / numero2
+multiplicacion = numero1 * numero2
+modulo = numero1 % numero2
+divisionEntera = numero1 // numero2
+
+print("La suma es: ",suma)
+print(("La resta es: ",resta))
+print("La division es ",division)
+print("La multiplicacion es ",multiplicacion)
+print("El modulo es ",modulo)
+print("La division es ",divisionEntera)
+
+
+print(f"La suma de {numero1} y {numero2} es",str(suma))
